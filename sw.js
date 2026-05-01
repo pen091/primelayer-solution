30df3e0abcbd5422bcec2a27f89acfe6158b32a5
pen091 <95129990+pen091@users.noreply.github.com>
2026-05-01 15:21:05 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-self.addEventListener("install", e => self.skipWaiting());
-
-self.addEventListener("activate", e => {});
-
-self.addEventListener("notificationclick", e => {
-  e.notification.close();
-  e.waitUntil(clients.openWindow("/"));
-});
